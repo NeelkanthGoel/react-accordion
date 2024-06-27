4d18ed58d0119926d831d4ad4647e2e8e54c38b1
NeelkanthGoel <goelneelkanth@gmail.com>
2024-06-27 13:42:12 -0700

    implemented the accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import style from './App.module.scss';
+import Accordion from './component/accordion';
+import {userDataType} from './types/types';
+
+
 
 function App() {
+
+  const [data, setData] = useState<userDataType | []>([]);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/NeelkanthGoel/react-accordion/demo/src/data/questionAnswerList.json')
+    .then(result => result.json())
+    .then(data => {
+      setData(data?.data ?? []);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }, [])
 
   return (
     <div className={style.container}>
@@ -12,11 +30,16 @@ function App() {
         <section className={style.question}>
           <h2>
             <span>Question:</span>
-            <span>You need to implement the accordion as shown below.</span>
+            <span>You need to implement the accordion as shown below. You can fetch the data for the accordion using the link:
+              <a target='_blank' href=' https://raw.githubusercontent.com/NeelkanthGoel/react-accordion/demo/src/data/questionAnswerList.json'> https://raw.githubusercontent.com/NeelkanthGoel/react-accordion/demo/src/data/questionAnswerList.json</a>
+            </span>
           </h2>
         </section>
         <section className={style.answer}>
-
+          <h2>Answer:</h2>
+          { data.length > 0 ? <Accordion data={data}/>
+          : <h2>data if fetching</h2>
+          }
         </section>
     </div>
   )
